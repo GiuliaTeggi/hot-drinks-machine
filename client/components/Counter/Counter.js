6b62c8be7a36c1389f5bf99ef30b0cadd840638d
Giulia <teggi.giulia@gmail.com>
2018-07-05 15:12:14 +0100

    Change countDown to countUp

--- client/components/Counter/Counter.js
+++ client/components/Counter/Counter.js
@@ -4,38 +4,32 @@ import propTypes from 'prop-types';
 export default class Counter extends React.Component {
   state = {
     count: '',
-    ready: false
+    total: ''
   }
 
-  componentDidMount(){
-    console.log("Component mounted");
-  }
   componentWillReceiveProps(nextProps) {
-    const { start, steps } = nextProps;
-    if (start === true && steps != null) {
+    const { steps } = nextProps;
+    if (steps != null) {
       this.clear();
-      console.log("Steps", steps.length)
-      const { count } = this.state;
-      this.setState({ count: steps.length }, () => {
-        this.countDown();
+      this.setState({ count: 1, total: steps.length }, () => {
+        this.countUp();
       });
     }
   }
 
-  countDown = () => {
-    const { count } = this.state;
+  countUp = () => {
+    const { count, total } = this.state;
     let time = count;
-      if (time > 0){
+      if (time < total ){
       this.interval = setInterval(() => {
-      time--
-      if (time === 0){this.clear()}
+      time++
+      if (time === total + 1){this.clear()}
       else this.setState({ count: time })}, 1000)
      }
 
   }
   
   clear = () => {
-    console.log("clear reached")
     clearInterval(this.interval);
     this.setState({ count: 0 });
   }
